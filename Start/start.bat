248751d781a8948d0eb836cbb1af7f6facb6c707
Tian Ba <2716615115@qq.com>
2025-12-16 09:34:00 +0800

    换个环境

--- Start/start.bat
+++ Start/start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Going TianBa AI Backend...
-start "TianBa AI Backend" cmd /k "cd /d "%~dp0..\Code" && call conda activate DICK && C:\Users\admin\.conda\envs\DICK\python.exe app\main\main.py"
+start "TianBa AI Backend" cmd /k "cd /d "%~dp0..\Code" && call conda activate DOGE && D:\Conda\envs\DOGE\python.exe app\main\main.py"
 
 timeout /t 2 /nobreak >nul
 
